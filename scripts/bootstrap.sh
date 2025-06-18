91d2f63815d2648db32f68f7e59f81ab95719755
smohite25 <surendra.mohite@brainwaveconsulting.co.in>
2025-06-18 12:30:40 +0530

    Update bootstrap.sh
    Corrected download path for Fabric and Fabric-CA by removing ../download/ from downloading path
    
    Signed-off-by: smohite25 <surendra.mohite@brainwaveconsulting.co.in>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -96,7 +96,7 @@ download() {
 
 pullBinaries() {
     echo "===> Downloading version ${FABRIC_TAG} platform specific fabric binaries"
-    download "${BINARY_FILE}" "https://github.com/hyperledger/fabric/releases/download/v${VERSION}/${BINARY_FILE}"
+    download "${BINARY_FILE}" "https://github.com/hyperledger/fabric/releases/v${VERSION}/${BINARY_FILE}"
     if [ $? -eq 22 ]; then
         echo
         echo "------> ${FABRIC_TAG} platform specific fabric binary is not available to download <----"
@@ -105,7 +105,7 @@ pullBinaries() {
     fi
 
     echo "===> Downloading version ${CA_TAG} platform specific fabric-ca-client binary"
-    download "${CA_BINARY_FILE}" "https://github.com/hyperledger/fabric-ca/releases/download/v${CA_VERSION}/${CA_BINARY_FILE}"
+    download "${CA_BINARY_FILE}" "https://github.com/hyperledger/fabric-ca/releases/v${CA_VERSION}/${CA_BINARY_FILE}"
     if [ $? -eq 22 ]; then
         echo
         echo "------> ${CA_TAG} fabric-ca-client binary is not available to download  (Available from 1.1.0-rc1) <----"
